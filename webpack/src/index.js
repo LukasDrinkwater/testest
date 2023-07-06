e597fd594b2a892a8c8d5a018822190b17af1d1b
LukasDrinkwater <93930287+LukasDrinkwater@users.noreply.github.com>
2023-07-06 12:53:12 +0100

    Initial commit

---
+++ webpack/src/index.js
@@ -0,0 +1,7 @@
+import _ from "lodash";
+import "./styles.css";
+
+// class that is the project and tasks for the todo list can be added to it.
+
+// use classes or a factory function to create each item that is added to
+// the specific project todo list.
